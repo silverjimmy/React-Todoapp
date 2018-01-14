e62619d53b44b34386e1749f5ac05528f09a85d1
silverjimmy <silverjimmy2@gmail.com>
2018-01-14 23:35:05 +0300

    Initial files for React-Todo-App

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -5,22 +5,25 @@ export default class todoInput extends React.Component{
     constructor(props){
         super(props);
 
-        this.state ={value: "test"};
+        this.state ={value: ''};
 
         this.handleChange = this.handleChange.bind(this);
         this.addTodo = this.addTodo.bind(this);
     }
 
-    handleChange(){
-        console.log("Change here");
+    handleChange(e){
+        this.setState({value: e.target.value});
     }
     addTodo(todo){
-        console.log("TODO: ",todo);
+        if (todo.length>0){
+        this.props.addTodo(todo);
+        this.setState({value: ''});
+        }
     }
     render(){
         return(
             <div>
-                <input type="text" value="" onChange={this.handleChange}/>
+                <input type="text" value={this.state.value} onChange={this.handleChange}/>
                 <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
             </div>
         );
